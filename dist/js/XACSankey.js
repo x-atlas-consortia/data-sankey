f4be97a97efa550c7098b06a885c7f7f3da916ab
Lisa-Ann B <lisabruney@pitt.edu>
2025-04-08 14:13:17 -0400

    Bump to 1.0.2

--- dist/js/XACSankey.js
+++ dist/js/XACSankey.js
@@ -1,6 +1,6 @@
 /**
 * 
-* 4/8/2025, 1:04:19 PM | X Atlas Consortia Sankey 1.0.1c | git+https://github.com/x-atlas-consortia/data-sankey.git | Pitt DBMI CODCC
+* 4/8/2025, 2:12:32 PM | X Atlas Consortia Sankey 1.0.2 | git+https://github.com/x-atlas-consortia/data-sankey.git | Pitt DBMI CODCC
 **/
 function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
