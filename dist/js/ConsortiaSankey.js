e356a49e47050fab1c4c41ee1735370d6cceff8f
Lisa-Ann B <115558393+libpitt@users.noreply.github.com>
2026-07-23 16:00:20 -0400

    Bump version of xac-sankey

---
+++ dist/js/ConsortiaSankey.js
@@ -0,0 +1,2 @@
+/** 7/23/2026, 3:59:59 PM | X Atlas Consortia Sankey 1.1.0 | git+https://github.com/x-atlas-consortia/data-sankey.git **/
+import*as d3 from"https://cdn.jsdelivr.net/npm/d3@7/+esm";import{sankey as d3sankey,sankeyLinkHorizontal}from"https://cdn.jsdelivr.net/npm/d3-sankey@0.12.3/+esm";import XACSankey from"./XACSankey.js";class ConsortiaSankey extends XACSankey{constructor(){super(),this.d3={d3,d3sankey,sankeyLinkHorizontal}}}customElements.define("consortia-sankey",ConsortiaSankey);var ConsortiaSankey_default=ConsortiaSankey;export{ConsortiaSankey_default as default};
---
+++ dist/js/ReactConsortiaSankey.js
@@ -0,0 +1,2 @@
+/** 7/23/2026, 3:59:59 PM | X Atlas Consortia Sankey 1.1.0 | git+https://github.com/x-atlas-consortia/data-sankey.git **/
+import*as d3 from"d3";import{sankey as d3sankey,sankeyLinkHorizontal}from"d3-sankey";import XACSankey from"./XACSankey.js";class ReactConsortiaSankey extends XACSankey{constructor(){super(),this.d3={d3,d3sankey,sankeyLinkHorizontal}}}customElements.define("react-consortia-sankey",ReactConsortiaSankey);var ReactConsortiaSankey_default=ReactConsortiaSankey;export{ReactConsortiaSankey_default as default};
