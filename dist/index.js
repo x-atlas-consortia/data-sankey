945000f8b282850e3021256cadafb4df91c7e131
Lisa-Ann B <115558393+libpitt@users.noreply.github.com>
2026-07-23 16:29:45 -0400

    Bump version

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,2 @@
-/** 7/23/2026, 4:25:23 PM | X Atlas Consortia Sankey 1.1.1 | git+https://github.com/x-atlas-consortia/data-sankey.git **/
+/** 7/23/2026, 4:29:31 PM | X Atlas Consortia Sankey 1.1.1 | git+https://github.com/x-atlas-consortia/data-sankey.git **/
 import XACSankey from"./js/XACSankey";import ReactConsortiaSankey from"./js/ReactConsortiaSankey";import SenNetAdapter from"./js/adapters/SenNetAdapter";import HuBMAPAdapter from"./js/adapters/HuBMAPAdapter";import Palette from"./js/util/Palette";import Util from"./js/util/Util";export{HuBMAPAdapter,Palette,ReactConsortiaSankey,SenNetAdapter,Util,XACSankey};
--- dist/js/ReactConsortiaSankey.js
+++ dist/js/ReactConsortiaSankey.js
@@ -1,2 +1,2 @@
-/** 7/23/2026, 4:25:23 PM | X Atlas Consortia Sankey 1.1.1 | git+https://github.com/x-atlas-consortia/data-sankey.git **/
+/** 7/23/2026, 4:29:31 PM | X Atlas Consortia Sankey 1.1.1 | git+https://github.com/x-atlas-consortia/data-sankey.git **/
 import*as d3 from"d3";import{sankey as d3sankey,sankeyLinkHorizontal}from"d3-sankey";import XACSankey from"./XACSankey.js";class ReactConsortiaSankey extends XACSankey{constructor(){super(),this.d3={d3,d3sankey,sankeyLinkHorizontal}}}customElements.define("react-consortia-sankey",ReactConsortiaSankey);var ReactConsortiaSankey_default=ReactConsortiaSankey;export{ReactConsortiaSankey_default as default};
